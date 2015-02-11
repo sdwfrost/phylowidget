cb852a364c6241ffd0239feab00eb3fd36ba626c
timelyportfolio <kent.russell@timelyportfolio.com>
2015-02-11 12:14:33 -0600

    add some basic documentation

---
+++ R/phylowidget-package.R
@@ -0,0 +1,13 @@
+#' phylowidget: htmlwidget wrapper for \href{https://github.com/veg/phylotree.js}{phylotree.js}
+#'
+#' phylowidget offers an interactive view of phylo data directly from R by
+#' wrapping the Javascript library \href{https://github.com/veg/phylotree.js}{phylotree.js}
+#' with the convenience and flexibility of an R \code{\href{http://htmlwidgets.org}{htmlwidget}}.
+#'
+#' To learn more about phylowidget, start with the vignettes:
+#' \code{browseVignettes(package = "phylowidget")}
+#'
+#' @name phylowidget-package
+#'
+#' @import htmlwidgets
+NULL

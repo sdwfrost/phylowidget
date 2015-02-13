d0303c69381e0de0e5cb8b9643e608181d555fd9
Simon Frost <sdwfrost@gmail.com>
2015-02-13 12:22:27 +0000

    Editing documentation

--- R/phylowidget-package.R
+++ R/phylowidget-package.R
@@ -1,8 +1,9 @@
 #' phylowidget: htmlwidget wrapper for \href{https://github.com/veg/phylotree.js}{phylotree.js}
 #'
-#' phylowidget offers an interactive view of phylo data directly from R by
+#' phylowidget offers an interactive view of a phylogeny directly from R by
 #' wrapping the Javascript library \href{https://github.com/veg/phylotree.js}{phylotree.js}
-#' with the convenience and flexibility of an R \code{\href{http://htmlwidgets.org}{htmlwidget}}.
+#' by Sergei Kosakovsky Pond and Steven Weaver. This combines the interactivity
+#' of D3.js with the convenience and flexibility of an R \href{http://htmlwidgets.org}{htmlwidget}.
 #'
 #' To learn more about phylowidget, start with the vignettes:
 #' \code{browseVignettes(package = "phylowidget")}
